e0e996f876c1678a8ac7c83938b88827414d98bc
TheoDlcq <theo2clercq@gmail.com>
2026-04-28 13:30:00 +0200

    test(RED): Ajout du test pour l'heure pile du matin

---
+++ AssistantVocal.Tests/TimeTranslatorTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssistantVocal;
+using System;
+
+namespace AssistantVocal.Tests
+{
+    [TestClass]
+    public class TimeTranslatorTest
+    {
+        [TestMethod]
+        public void GetTimeAsText_HeurePileMatin_RetourneTexte()
+        {
+            var date = new DateTime(2026, 4, 28, 7, 0, 0);
+            Assert.AreEqual("sept heures du matin", TimeTranslator.GetTimeAsText(date));
+        }
+    }
+}
