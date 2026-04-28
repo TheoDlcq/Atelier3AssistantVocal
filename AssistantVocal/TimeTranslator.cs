ce3188c8fb8b8b310b756304e90ba5f758c6687b
TheoDlcq <theo2clercq@gmail.com>
2026-04-28 13:45:00 +0200

    refactor: Utilisation d'un tableau pour gerer toutes les heures du matin

--- AssistantVocal/TimeTranslator.cs
+++ AssistantVocal/TimeTranslator.cs
@@ -4,9 +4,14 @@ namespace AssistantVocal
 {
     public static class TimeTranslator
     {
+        private static readonly string[] Heures = { 
+            "minuit", "une", "deux", "trois", "quatre", "cinq", "six", 
+            "sept", "huit", "neuf", "dix", "onze", "midi" 
+        };
+
         public static string GetTimeAsText(DateTime time)
         {
-            return "sept heures du matin";
+            return $"{Heures[time.Hour]} heures du matin";
         }
     }
 }
