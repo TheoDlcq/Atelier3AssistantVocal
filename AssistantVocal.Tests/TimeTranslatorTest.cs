cbdfd716beaee5389a590bb4d2457feaa9f25d87
TheoDlcq <theo2clercq@gmail.com>
2026-04-28 14:00:00 +0200

    test(RED): Ajout du test pour l'heure pile de l'apres-midi

--- AssistantVocal.Tests/TimeTranslatorTest.cs
+++ AssistantVocal.Tests/TimeTranslatorTest.cs
@@ -13,5 +13,12 @@ namespace AssistantVocal.Tests
             var date = new DateTime(2026, 4, 28, 7, 0, 0);
             Assert.AreEqual("sept heures du matin", TimeTranslator.GetTimeAsText(date));
         }
+
+        [TestMethod]
+        public void GetTimeAsText_HeurePileApresMidi_RetourneTexte()
+        {
+            var date = new DateTime(2026, 4, 28, 14, 0, 0);
+            Assert.AreEqual("deux heures de l'après-midi", TimeTranslator.GetTimeAsText(date));
+        }
     }
 }
