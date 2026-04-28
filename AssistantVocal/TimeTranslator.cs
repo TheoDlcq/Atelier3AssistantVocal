4ef8ce977d2c27f69533aa3aff68a487b6a54abd
TheoDlcq <theo2clercq@gmail.com>
2026-04-28 14:45:00 +0200

    feat(GREEN): Implementation de la logique pour intercepter midi et minuit

--- AssistantVocal/TimeTranslator.cs
+++ AssistantVocal/TimeTranslator.cs
@@ -13,6 +13,13 @@ namespace AssistantVocal
         {
             int hour = time.Hour;
             
+            // Interception des cas spéciaux "pile"
+            if (time.Minute == 0)
+            {
+                if (hour == 12) return "midi";
+                if (hour == 0) return "minuit";
+            }
+
             string periode = hour < 12 ? "du matin" : "de l'après-midi";
             int displayHour = hour > 12 ? hour - 12 : hour;
             
